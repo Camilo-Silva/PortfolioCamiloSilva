1a3a1102cefd1dc595d5d702bf3e6cae9c06cb09
Camilo-Silva <camilosilva.0301@gmail.com>
2026-01-28 10:14:22 -0300

    Add: Proyecto de Automatización de Documentos Funcionales

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -42,71 +42,90 @@ export class ProjectsService {
     return [
       {
         id: '1',
+        nombre: 'Automatización Documentos Funcionales',
+        descripcion: 'Aplicación de escritorio desarrollada en Python que automatiza la extracción de requerimientos funcionales desde una intranet institucional mediante técnicas avanzadas de web scraping. Esta solución optimiza significativamente los procesos de los analistas funcionales al automatizar la creación de archivos .docx estandarizados, eliminando completamente la carga manual de datos y reduciendo drásticamente el tiempo de confección de documentos técnicos. La aplicación garantiza consistencia en la documentación y permite a los analistas enfocarse en tareas de mayor valor agregado.',
+        descripcionCorta: 'Aplicación Python de escritorio que automatiza la extracción de requerimientos mediante scraping y genera documentos .docx estandarizados, optimizando el trabajo de analistas funcionales.',
+        videos: [
+          'https://drive.google.com/file/d/1gBcJfihj3RMN0_0mPmd6vCxC3IX5ksaT/view?usp=sharing'
+        ],
+        // imagenes: [],
+        imagenes: [],
+        tecnologias: ['Python', 'Web Scraping', 'Beautiful Soup', 'Selenium', 'Python-docx', 'Tkinter', 'Requests'],
+        githubUrl: 'https://github.com/Camilo-Silva/AutomatizacionIntranetEmpresaTecnologia.git',
+        hostedUrl: 'https://drive.google.com/file/d/1gBcJfihj3RMN0_0mPmd6vCxC3IX5ksaT/view?usp=sharing',
+        thumbnailUrl: '/assets/images/project6.png'
+      },
+      {
+        id: '2',
         nombre: 'Sublisa',
         descripcion: 'Tienda online especializada en el rubro de la sublimación. Ofrece una amplia gama de productos que incluyen artículos sublimables e insumos para profesionales, así como opciones de artículos personalizados y tecnología DTF. El proyecto está diseñado para que los usuarios puedan explorar fácilmente el catálogo, visualizar los productos y agregar los elementos deseados a un carrito de compras para finalizar su pedido de manera eficiente.',
         descripcionCorta: 'Tienda online especializada en sublimación que vende artículos sublimables, insumos para profesionales y productos personalizados (DTF), con carrito de compras para finalizar pedidos.',
         videos: [
           'https://drive.google.com/file/d/1idfxgHcMyvl4N39SzQHFHHNH3RovnNmr/view?usp=sharing'
         ],
-        imagenes: [
-          '/assets/images/project5-1.jpg',
-          '/assets/images/project5-2.jpg',
-          '/assets/images/project5-3.jpg'
-        ],
+        // imagenes: [
+        //   '/assets/images/project5-1.jpg',
+        //   '/assets/images/project5-2.jpg',
+        //   '/assets/images/project5-3.jpg'
+        // ],
+        imagenes: [],
         tecnologias: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'TypeScript', 'API REST', 'Supabase', 'PostgreSQL'],
         githubUrl: 'https://github.com/Camilo-Silva/Sublisa.git',
         hostedUrl: 'https://sublisa.netlify.app/',
         thumbnailUrl: '/assets/images/project5.png'
       },
       {
-        id: '2',
+        id: '3',
         nombre: 'Way Combat',
         descripcion: 'Desarrollada como una plataforma web robusta para el fitness de combate. El backend fue implementado con ASP.NET Core, manejando eficientemente el registro de usuarios y la distribución de los mixes de entrenamiento. El frontend, creado con Angular, proporciona una experiencia de usuario fluida y reactiva para navegar y disfrutar de las rutinas.',
         descripcionCorta: 'Aplicación web de fitness de combate, donde usuarios pueden adquirir una variedad de mixes de entrenamiento.',
         videos: [
           'https://drive.google.com/file/d/1tCO456HFhG2kv_CD8Fs8RawcNT2NAGo5/view?usp=sharing'
         ],
-        imagenes: [
-          '/assets/images/project4-1.jpg',
-          '/assets/images/project4-2.jpg',
-          '/assets/images/project4-3.jpg'
-        ],
+        // imagenes: [
+        //   '/assets/images/project4-1.jpg',
+        //   '/assets/images/project4-2.jpg',
+        //   '/assets/images/project4-3.jpg'
+        // ],
+        imagenes: [],
         tecnologias: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'TypeScript', 'API REST', 'ASP.NET Core', 'Entity Framework Core', 'SQL Server'],
         githubUrl: 'https://github.com/Camilo-Silva/WayCombat.git',
         hostedUrl: 'https://way-combat.netlify.app/',
         thumbnailUrl: '/assets/images/project4.png'
       },
       {
-        id: '3',
+        id: '4',
         nombre: 'Spotify Clon',
         descripcion: 'Este proyecto es un clon de Spotify desarrollado con Angular 17.3, que implementa un reproductor de música, gestión de usuarios, historial, favoritos y navegación modular. El objetivo es practicar arquitectura escalable, buenas prácticas y el uso de Angular moderno (standalone components, directivas y pipes. Integración con API deployada en https://api-reproductormusica-production.up.railway.app/ ).',
         descripcionCorta: 'Clon de Spotify con Angular 17.3. Reproductor de música con gestión de usuarios, favoritos e historial.',
         videos: [
           'https://drive.google.com/file/d/1P3iIVv51_hI7S_4CI4pojN5PoWZnADmc/view?usp=sharing'
         ],
-        imagenes: [
-          '/assets/images/project1-1.jpg',
-          '/assets/images/project1-2.jpg',
-          '/assets/images/project1-3.jpg'
-        ],
+        // imagenes: [
+        //   '/assets/images/project1-1.jpg',
+        //   '/assets/images/project1-2.jpg',
+        //   '/assets/images/project1-3.jpg'
+        // ],
+        imagenes: [],
         tecnologias: ['Angular', 'TypeScript', 'SCSS', 'RxJS', 'Angular Router', 'API REST'],
         githubUrl: 'https://github.com/Camilo-Silva/AppReproductorMusica.git',
         hostedUrl: 'https://spotify-clon-camilo-silva.netlify.app/',
         thumbnailUrl: '/assets/images/project1.png'
       },
       {
-        id: '4',
+        id: '5',
         nombre: 'App de Gestión de Turnos',
         descripcion: 'es un sistema integral de gestión de turnos médicos desarrollado con ASP.NET Core 8.0 MVC. La aplicación facilita la administración de citas médicas, permitiendo la gestión de pacientes, médicos, especialidades, turnos y historiales médicos de manera eficiente y centralizada.',
         descripcionCorta: 'Sistema de gestión de turnos médicos con ASP.NET Core. Administración completa de citas y pacientes.',
         videos: [
           'https://drive.google.com/file/d/1Hg3jxrDwXmmD1Gp6npi6N83S74ji31YX/view?usp=sharing'
         ],
-        imagenes: [
-          '/assets/images/project2-1.jpg',
-          '/assets/images/project2-2.jpg',
-          '/assets/images/project2-3.jpg'
-        ],
+        // imagenes: [
+        //   '/assets/images/project2-1.jpg',
+        //   '/assets/images/project2-2.jpg',
+        //   '/assets/images/project2-3.jpg'
+        // ],
+        imagenes: [],
         tecnologias: ['ASP.NET Core', 'PostgreSQL (Railway)', 'Entity Framework Core', 'ASP.NET Core Identity', 'MVC con Razor Views', 'Docker'],
         githubUrl: 'https://github.com/Camilo-Silva/TurnoSmart',
         hostedUrl: 'https://turnosmart-production.up.railway.app/',
@@ -114,18 +133,19 @@ export class ProjectsService {
         hasFlowDetails: true // Este proyecto tiene detalles de flujo de app
       },
       {
-        id: '5',
+        id: '6',
         nombre: 'Calculadora Python',
         descripcion: 'Una calculadora web moderna y funcional desarrollada con Python Flask que incluye: Calculadora Básica: Operaciones aritméticas fundamentales - Calculadora Científica: Funciones matemáticas avanzadas - Operaciones con Listas: Cálculos con múltiples números - Interfaz Moderna: Diseño responsivo y atractivo - Historial: Registro de todos los cálculos realizados - Soporte de Teclado: Navega usando el teclado',
         descripcionCorta: 'Calculadora web moderna con Python Flask. Funciones básicas, científicas y historial de cálculos.',
         videos: [
           'https://drive.google.com/file/d/1MXfqPyzFrERNddvFfs_WSaBFfJMszziB/view?usp=sharing'
         ],
-        imagenes: [
-          '/assets/images/project3-1.jpg',
-          '/assets/images/project3-2.jpg',
-          '/assets/images/project3-3.jpg'
-        ],
+        // imagenes: [
+        //   '/assets/images/project3-1.jpg',
+        //   '/assets/images/project3-2.jpg',
+        //   '/assets/images/project3-3.jpg'
+        // ],
+        imagenes: [],
         tecnologias: ['Python', 'Flask', 'JavaScript', 'HTML5', 'CSS3', 'API REST', 'Railway'],
         githubUrl: 'https://github.com/Camilo-Silva/CalculadoraPython.git',
         hostedUrl: 'https://calculadorapython-production.up.railway.app/',
